2a11dbbfb4948f666e715ce43bb93efacbc47f8f
SammY2008 <88900622+SammY2008@users.noreply.github.com>
2021-09-26 19:01:36 -0500

    Add files via upload

---
+++ Plantilla-Proyecto-C28-main/Plantilla-Proyecto-C28-main/launcher.js
@@ -0,0 +1,30 @@
+class launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.004,
+            length: 40
+        }
+        this.launcher = Constraint.create(options);
+        this.pointB=pointB;
+        World.add(world, this.launcher);
+    }
+    attach(body){
+        this.launcher.bodyA=body;
+    }
+    fly(){
+
+        this.launcher.bodyA = null;
+    }
+    display(){
+        if(this.launcher.bodyA){
+        var pointA = this.launcher.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        stroke("green");
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+}
